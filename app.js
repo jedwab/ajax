479e775b206e0933a3e72c1426208b8541466d0a
Justyna Bobrowska <justynabobrowska88@gmail.com>
2018-01-29 20:05:05 +0100

    new kanban

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '2689',
+  'X-Auth-Token': '4c07aa90171fc1a802e961cf3dbf1046'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
 }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
 
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
--- column.js
+++ column.js
@@ -1,8 +1,8 @@
-function Column(name) {
+function Column(id, name) {
 	var self = this;
 	
-	this.id = randomString();
-	this.name = name;
+	this.id = id
+	this.name = name || "No name given";
 	this.element = createColumn();
 
 	function createColumn() {
@@ -19,9 +19,22 @@ function Column(name) {
 		});
 		
 		columnAddCard.click(function(event) {
+			var cardName = prompt("Enter the name of the card");
 			event.preventDefault();
-			self.createCard(new Card(prompt("Wpisz nazwę karty")));
+			$.ajax({
+       			url: baseUrl + '/card',
+       			method: 'POST',
+        		data: {
+        		name: cardName,
+   				bootcamp_kanban_column_id: self.id
+    			},
+    			success: function(response) {
+        			var card = new Card(response.id, cardName);
+        			self.createCard(card);
+    			}
+			});	
 		});
+
 			
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
 		column.append(columnTitle)
@@ -36,6 +49,13 @@ Column.prototype = {
 	  this.element.children('ul').append(card.element);
 	},
 	deleteColumn: function() {
-	  this.element.remove();
-	}
+		var self = this;
+    	$.ajax({
+	      url: baseUrl + '/column/' + self.id,
+	      method: 'DELETE',
+	      success: function(response){
+	        self.element.remove();
+      		}
+    	});
+ 	}
 };
