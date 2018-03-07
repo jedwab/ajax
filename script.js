68e5c90222e7b3ff705ce4b19658d97fdf508613
Justyna Bobrowska <justynabobrowska88@gmail.com>
2018-03-07 18:20:18 +0100

    add styles and new features

--- script.js
+++ script.js
@@ -2,26 +2,72 @@ var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
 
 $('#search').click(searchCountries);
+$('#country-name').keypress(function(e) {
+	if(e.which == 13) {
+		searchCountries();
+	}
+});
+
 
 function searchCountries() {
  	var countryName = $('#country-name').val();
-if(!countryName.length) countryName = 'Poland';
+
 $.ajax({
   		url: url + countryName,
   		method: 'GET',
-  		success: showCountriesList
-  	});
+		success: showCountriesList,
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			notFound();
+		}
+		  
+	  });
+	  function notFound () {
+		  alert("Country not found, try again");
+	  }
 }
 
 function showCountriesList(resp) {
 	countriesList.empty();
-	var countryName = $('#country-name').val();
-
-	var filteredCountries = resp.filter(function(singleCountry){  
-       return singleCountry.name.toLowerCase().indexOf(countryName.toLowerCase()) != -1;
-    });
-
-    filteredCountries.forEach(function(item) {
-    	$('<li>').text(item.name).appendTo(countriesList);
-    });
+	$('#country-name').val('');
+	resp.forEach(function(item) {
+		countriesList.append(
+			`<div class="wrapper">
+				<div class="header">
+					<h3>${item.name}</h3>
+				</div>
+				<div>
+					<h4>Additional Information</h4>
+					<div class="row">
+						<div class="col-sm-6">Capital:</div>
+						<div class="col-sm-6">${item.capital}</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">Native Name:</div>
+						<div class="col-sm-6">${item.nativeName}</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">Population:</div>
+						<div class="col-sm-6">${item.population + " people"}</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">Region:</div>
+						<div class="col-sm-6">${item.region},  ${item.subregion}</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">Area:</div>
+						<div class="col-sm-6">${item.area + " km"}</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">Currency:</div>
+						<div class="col-sm-6">${item.currencies}</div>
+					</div>
+				</div>
+			</div>`
+		);
+		});
 }
+
+
+
+
+
